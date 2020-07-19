ddd01c8ff28569213566cb79344858c4ae17a4ba
lukeemail <kjd1760@gmail.com>
2020-07-19 14:39:31 +0800

    refactor: courses#show

--- spec/controllers/courses_controller_spec.rb
+++ spec/controllers/courses_controller_spec.rb
@@ -11,40 +11,31 @@ RSpec.describe CoursesController do
       expect(assigns[:courses]).to eq([course1, course2])
     end
 
-  describe "GET show" do
+    it "render template" do 
+      course1 = create(:course)
+      course2 = create(:course)
+
+      get :index
+
+      expect(response).to render_template("index")
+    end
+  end
+
+  describe "GET show" do 
     it "assigns @course" do
       course = create(:course)
 
-      get :show, params: { id: course.id}
+      get :show, params: {id: course.id}
 
       expect(assigns[:course]).to eq(course)
     end
-  end
-
-  describe "GET new" do
-    it "assign @course" do
-      course = build(:course)
-
-      get :new
-
-      expect(assigns(:course)).to be_a_new(Course)
-    end
-  end
 
     it "render template" do
-      course1 = create(:course)
-      course2 = create(:course)
       course = create(:course)
-      course = build(:course)
-      
-      # get :index
-      # get :show, params: {id: course.id}
-      get :new
-      
 
-      # expect(response).to render_template("index")
-      # expect(response).to render_template("show")
-      expect(response).to render_template("new")
+      get :show, params: {id: course.id}
+
+      expect(response).to render_template("show")
     end
   end
 end
